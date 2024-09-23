e0e171dccf434f56685a57f133ec1703529065ac
nezinauuu <B00133471@mytudublin.ie>
2024-09-23 11:51:29 +0100

    8
    Slime enemy

--- wildlands2/script/arrow.gd
+++ wildlands2/script/arrow.gd
@@ -9,7 +9,8 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
 	
-
+func arrow_deal_damage():
+	pass
 
 
 func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
